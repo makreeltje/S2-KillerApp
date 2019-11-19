a48e92dcd5771393d9f28569c0e9dd1842ecbd38
Rick Meels <rick.meels@gmail.com>
2019-11-19 15:11:25 +0100

    - Created all class files in business and data layer

--- KillerApp/KillerApp.Business/Movie.cs
+++ KillerApp/KillerApp.Business/Movie.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KillerApp.Business
+namespace KillerApp.Data
 {
-    public class Class1
+    class Movie
     {
     }
 }
