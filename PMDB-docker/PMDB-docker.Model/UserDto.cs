3304d9d45c0cc00c98a1f2f866fe11467a846101
Rick Meels <rick.meels@gmail.com>
2019-12-11 21:23:50 +0100

    User control
    - Removed UserDTO from ignore list and added it to the project

---
+++ PMDB-docker/PMDB-docker.Model/UserDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PMDB_docker.Models
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfRegistration { get; set; }
+        public string ProfileImage { get; set; }
+        public string Gender { get; set; }
+    }
+}
