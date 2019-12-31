31e1a199ce707208606cbfa64f2d0a74d4aa6e91
Rick Meels <rick.meels@gmail.com>
2019-12-31 02:08:27 +0100

    People
    - Implemented people feature

---
+++ PMDB-docker/PMDB-docker.Model/PeopleDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMDB_docker.Models
+{
+    public class PeopleDto
+    {
+        //public enum Gender
+        //{
+        //    Unknown,
+        //    Female,
+        //    Male
+        //}
+        public int Id { get; set; }
+        public string Character { get; set; }
+        public string CreditId { get; set; }
+        public int Gender { get; set; }
+        public int PeopleId { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public string ProfilePath { get; set; }
+    }
+}
