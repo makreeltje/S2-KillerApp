a9d653c5b381e67e02b167b923d471995c4406a2
Rick Meels <rick.meels@gmail.com>
2020-01-05 22:57:44 +0100

    Target framework and RoleDto
    - Target framework has been changed to 3.1 instead of 3.0
    - Role dto has been removed from the gitignore

---
+++ PMDB-docker/PMDB-docker.Model/RoleDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace PMDB_docker.Models
+{
+    public class RoleDto
+    {
+        //public enum Gender
+        //{
+        //    Unknown,
+        //    Female,
+        //    Male
+        //}
+        public int Id { get; set; }
+        public int TmdbId { get; set; }
+        public string Name { get; set; }
+        public string Character { get; set; }
+        public int? Order { get; set; }
+        public string Department { get; set; }
+        public string Job { get; set; }
+        public string ProfilePath { get; set; }
+    }
+}
